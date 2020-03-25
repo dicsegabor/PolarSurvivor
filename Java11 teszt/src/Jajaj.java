89e0268e0ef181ce8d17e773123cee6c7360d489
Dicse Gábor <dicsegabor@gmail.com>
2020-03-25 11:34:55 +0100

    Tesztprojekt

--- Java11 teszt/src/Jajaj.java
+++ Java11 teszt/src/Jajaj.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Jajaj {
 
-    public class f{
-        
+    public static class f{
+
+        String s;
+
+        public static void main(String[] args){
+
+            System.out.println("JAjaj");
+
+            Scanner input = new Scanner(System.in);
+
+            
+        }
     }
 }
