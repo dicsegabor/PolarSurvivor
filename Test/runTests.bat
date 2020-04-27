d744d46cc95bb1c2ee4d536894725436a712a33c
Dicse Gábor <dicsegabor@gmail.com>
2020-04-27 02:48:59 +0200

    For ciklus feljavitva

--- Test/runTests.bat
+++ Test/runTests.bat
@@ -1,5 +1,14 @@
 @echo off
-for %%f in (test???.txt) do (
+for %%f in (test?.txt) do (
+	java -jar PolarSurvivor.jar %%f
+)
+for %%f in (test1?.txt) do (
+	java -jar PolarSurvivor.jar %%f
+)
+for %%f in (test2?.txt) do (
+	java -jar PolarSurvivor.jar %%f
+)
+for %%f in (test3?.txt) do (
 	java -jar PolarSurvivor.jar %%f
 )
 pause
